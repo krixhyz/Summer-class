22ffd869d660228f4129e58b2fc5f9a94587fe4f
krixhyz <165859642+krixhyz@users.noreply.github.com>
2025-07-03 09:01:05 +0545

    Day 2 project setup and navigation
    aaa

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\ContactController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']) ->name('home');
+
+Route::get('/news', [NewsController::class, 'news'])->name('news');
+Route::get('/contact', [ContactController::class, 'contact'])->name('contact');
+Route::get('/about', [AboutController::class, 'about'])->name('about');
